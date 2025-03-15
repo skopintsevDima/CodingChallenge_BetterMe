52ab3c73fd6772b069f08e61d3ce26104dead861
Your Name <you@example.com>
2025-03-15 13:56:14 +0100

    Setup GlobalExceptionHandler

--- base/src/main/java/app/bettermetesttask/AndroidTestTaskApp.kt
+++ base/src/main/java/app/bettermetesttask/AndroidTestTaskApp.kt
@@ -1,13 +1,16 @@
 package app.bettermetesttask
 
 import android.app.Application
+import android.widget.Toast
 import app.bettermetesttask.featurecommon.initializers.AppInitializers
 import app.bettermetesttask.featurecommon.injection.utils.AppInjector
 import app.bettermetesttask.injection.components.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
+import timber.log.Timber
 import javax.inject.Inject
+import kotlin.system.exitProcess
 
 class AndroidTestTaskApp : Application(), HasAndroidInjector {
 
@@ -18,12 +21,25 @@ class AndroidTestTaskApp : Application(), HasAndroidInjector {
 
     override fun onCreate() {
         super.onCreate()
+        setupGlobalExceptionHandler()
+
         AppInjector.init(this) {
             DaggerAppComponent.builder().application(this).appContext(this)
                 .build().inject(this)
         }
 
         appInitializers.init(this)
+    }
+
+    private fun setupGlobalExceptionHandler() {
+        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+            val errorMsg = "GlobalException: uncaught exception on thread ${thread.name}: $throwable"
+            Timber.e(errorMsg)
+
+            Toast.makeText(this, errorMsg, Toast.LENGTH_LONG).show()
+
+            exitProcess(1)
+        }
     }
 
     override fun androidInjector(): AndroidInjector<Any> = dispatchingAndroidInjector
